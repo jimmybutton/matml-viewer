ead1492336a9511eb91366a16a4f7703befa42d5
sliedtkegranta <39529148+sliedtkegranta@users.noreply.github.com>
2019-07-12 17:42:27 +0100

    some work on the parser and tests

--- matml/utils.py
+++ matml/utils.py
@@ -1,15 +1,41 @@
-import xml.etree.ElementTree as et
+from math import log10, floor
 
-def todict(matml_as_string):
-    root = et.fromstring(matml_as_string)
-    node_name = root.tag
-    node_value = " ".join([line.strip() for line in root.text.splitlines()]).strip()
-    node_attrib = root.attrib
-    data_dict = {}
+def round_sig(x, sig=2):
+    return round(x, sig-int(floor(log10(abs(x))))-1)
+
+def parse_text(some_string):
+    """Tries to convert string into integer or double, if not possible returns string.
+    String can be multiple lines, will be concatenated and stripped of whites spaces."""
+
+    try:
+        some_float = float(some_string)
+    except ValueError:
+        # if it cannot be converted into a float, it is probably a string
+        return " ".join([line.strip() for line in some_string.splitlines()]).strip()
+
+    try:
+        some_integer = int(some_float)
+    except ValueError:
+        pass
+
+    if round_sig(some_float,6) == some_integer:
+        # if the float is not different from the integer, return the integer
+        return some_integer
+    else:
+        return some_float
+
+def matml_to_dict(node):
+    node_name = node.tag
+    node_value = node.text
+    node_attrib = node.attrib
+    data_dict = dict()
     if node_attrib:
         for k, v in node_attrib.items():
             data_dict.update({"@{}".format(k): v})
-            data_dict.update({"#text": node_value})
+            if not (node_value == '' or node_value is None):
+                data_dict.update({"#text": parse_text(node_value)})
+        for child in node:
+            data_dict.update(matml_to_dict(child))
         return {node_name: data_dict}
     else:
         return {node_name: node_value}
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,20 @@
 import pytest
 from matml import utils
+import xml.etree.ElementTree as et
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4, compact=True)
+
+def test_parse_text():
+    assert utils.parse_text('123') == 123
+    assert utils.parse_text('1.234') == 1.234
+    assert utils.parse_text('    \n\r    My text     \n\r    ') == "My text"
 
 def test_node_with_text():
     source = "<textnode>Some text...</textnode>"
     target = {'textnode': "Some text..."}
-    temp = utils.todict(source)
+    node = et.fromstring(source)
+    temp = utils.matml_to_dict(node)
     assert temp == target
 
 def test_node_with_attrib():
@@ -19,5 +29,27 @@ def test_node_with_attrib():
             '#text': "Some text..."
         }
     }
-    temp = utils.todict(source)
+    node = et.fromstring(source)
+    temp = utils.matml_to_dict(node)
+    assert temp == target
+
+def test_node_with_children():
+    source = """<?xml version="1.0"?>
+    <country name="Liechtenstein">
+        <rank>1</rank>
+        <year>2008</year>
+        <gdppc>141100</gdppc>
+    </country>
+    """
+    target = {
+        'country': {
+            '@name': "Liechtenstein",
+            'rank': 1,
+            'year': 2008,
+            'gdppc': 141100,
+        }
+    }
+    node = et.fromstring(source)
+    temp = utils.matml_to_dict(node)
+    pp.pprint(temp)
     assert temp == target
