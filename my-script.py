664fe6437b09758962466d9e739ff34337c9fd23
sliedtkegranta <39529148+sliedtkegranta@users.noreply.github.com>
2019-07-25 17:04:41 +0100

    my-script prints properties from dict

--- my-script.py
+++ my-script.py
@@ -5,7 +5,26 @@ import pprint
 
 pp = pprint.PrettyPrinter(indent=4, compact=True)
 
-filename = "matml-examples/MatML3e1.xml"
+def parse_unit(units):
+    """Parses the MatML Units node with units u and power p into a string of the format u1p1u2p2u3p3..."""
+
+    # if units is not a list, put it in a list
+    units = units if isinstance(units, list) else [units]
+
+    unit_str = ''
+    for unit in units:
+        unit_name = unit.get('Name')
+        unit_power = unit.get('@power')
+        if not unit_str == '':
+            unit_str += '.'
+        if unit_name:
+            unit_str += unit_name
+        if unit_power:
+            unit_str += str(unit_power)
+    return unit_str
+
+# filename = "matml-examples/MatML3e1.xml"
+filename = "matml-examples/Zytel FG70G30 HSR2 - Creep 80C.xml"
 
 with open(filename, 'r', encoding="utf-8") as fp:
     orig_data = fp.read()
@@ -15,4 +34,34 @@ rootnode = et.fromstring(orig_data)
 # with open("sandbox/MatML3e1_own.json", 'w', encoding='utf-8') as fp:
 #     json.dump(mu.matml_to_dict(rootnode), fp, sort_keys=True, indent=4)
 
-print(json.dumps(mu.matml_to_dict(rootnode), sort_keys=True, indent=4, ensure_ascii=False))
+material = mu.matml_to_dict(rootnode)
+
+# print the data
+print(json.dumps(material, sort_keys=True, indent=4, ensure_ascii=False))
+
+material = material.get('EngineeringData').get('Materials')
+
+propertydata = material.get('MatML_Doc').get('Material').get('BulkDetails').get('PropertyData')
+propertydetails = material.get('MatML_Doc').get('Metadata').get('PropertyDetails')
+parameterdetails = material.get('MatML_Doc').get('Metadata').get('ParameterDetails')
+
+for pr in propertydata:
+    pr_id = pr.get('@property')
+    pr_det = [pr_det for pr_det in propertydetails if pr_det.get('@id') == pr_id][0]
+    pr_name = pr_det.get('Name')
+    pr_units = pr_det.get('Units')
+    pr_unit = parse_unit(pr_units.get('Unit')) if pr_units is not None else '[-]'
+    pr_value = pr.get('Data').get('#text')
+    print(pr_name, ':', pr_value, pr_unit)
+
+    parameters = pr.get('ParameterValue')
+    if parameters is not None:
+        parameters = parameters if isinstance(parameters, list) else [parameters]
+        for pa in parameters:
+            pa_id = pa.get('@parameter')
+            pa_det = [pa_det for pa_det in parameterdetails if pa_det.get('@id') == pa_id][0]
+            pa_name = pa_det.get('Name')
+            pa_units = pa_det.get('Units')
+            pa_unit = parse_unit(pa_units.get('Unit')) if pa_units is not None else '[-]'
+            pa_value = pa.get('Data')
+            print('   ', pa_name, ':', pa_value, pa_unit)
